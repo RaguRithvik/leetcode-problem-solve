a63d7ff5c90ba11f3673dfb2bd661e1742d900ea
ragurithvik <ragurithvik@gmail.com>
2024-10-22 09:40:05 +0530

    interview code

--- intLearn.js
+++ intLearn.js
@@ -1,3 +1,4 @@
+//------ 01-21-10-2024 ------
 const nestedArray = [2, 3, 4, [5, [6, 7, 8, [9, 10, 11], 12], 13], 14, 15];
 
 function flattenArray(arr) {
@@ -19,5 +20,43 @@ function flattenArray(arr) {
 //Explanation
 [0, 1, 2, 3,[4], 5, 6]
 const flatArray = flattenArray(nestedArray);
-console.log(flatArray);
+// console.log(flatArray);
+
+//------ 02-22-10-2024 ------
+const one = false || {} || null
+const two = null || "" || false
+const three = [] || 0 || true
+//Explanation
+//false null values are falsy value - {}
+//All falsy value go take last one - false
+//[] and true first value check first values is true so - [] 
+// console.log(one, two, three)
+
+//------ 03 - 02-22-2024 ------
+const data = [2, 3, 6, 8, 2, 10, 20, 1];
+let i = 0;
+let isStatus = true;
+const result = [];
+while (i < data.length) {
+  if (isStatus) {
+    const push = [];
+    for (j = 0; j < 3 && data[i + j]; j++) {
+      push[j] = data[i + j];
+    }
+    result[result.length] = push;
+    i = i + 3;
+    isStatus = false;
+  } 
+  else {
+    const push = [];
+    for (j = 0; j < 2 && data[i + j]; j++) {
+      push[j] = data[i + j];
+    }
+    result[result.length] = push;
+    i = i + 2;
+    isStatus = true;
+  }
+}
+[ [ 2, 3, 6 ], [ 8, 2 ], [ 10, 20, 1 ] ]
+// console.log(result, "result");
 
