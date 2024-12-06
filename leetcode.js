602ee257906096612ab1002f47df85124a5a6343
ragurithvik <ragurithvik@gmail.com>
2024-12-07 00:52:59 +0530

    list

--- leetcode.js
+++ leetcode.js
@@ -258,5 +258,81 @@ const longestCommonPrefix = (arr) => {
 console.log(longestCommonPrefix(["flower", "flow", "flight"])); // Output: "fl"
 // -----------------------------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------
-
-
+class LinkedList {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = null;
+  }
+}
+//print
+function printedResult(arr) {
+  const result = [];
+  while (arr) {
+    result.push(arr.data);
+    arr = arr.next;
+  }
+  return result;
+}
+//Nodelist
+function addTwo(listOne, listTwo) {
+  const node = new LinkedList(0);
+  let currry = 0;
+  let dummy = node;
+  while (listOne || listTwo || currry) {
+    let l1 = listOne.data || 0;
+    let l2 = listTwo.data || 0;
+    let sum = l1 + l2 + currry;
+    currry = Math.floor(sum / 10); //-->10/10= 1 5/10 = Math.floor(0.5) 0
+    const current = new LinkedList(sum % 10); //
+    //1+4=5%10 --> 5
+    //7+3=10%10 --> 0
+    //7+4 =11%10-->1 //only added 11 right value 1
+    dummy.next = current;
+    dummy = current;
+    listOne = listOne?.next || null; // next get data
+    listTwo = listTwo?.next || null;
+  }
+  return node.next;
+}
+//getLinkedList
+function getList(arr) {
+  const node = new LinkedList(0);
+  let dummy = node;
+  for (let num of arr) {
+    const current = new LinkedList(num); //get current value
+    dummy.next = current; //push current value to dummy.next
+    dummy = current; //again dummy = current
+    //{data:0 next: null}-->dummy
+    //{data:0 next: {data:1 next: null}}
+    //which item to push next value so that dummy now current
+  }
+  return node.next;
+  //LinkedList {data: 1,next: LinkedList { data: 2, next: LinkedList { data: 3, next: null } }} listOne
+}
+const listOne = getList([1, 2, 3]);
+const listTwo = getList([4, 5, 6]);
+const result = addTwo(listOne, listTwo);
+// console.log(printedResult(result));
+//leetcodes-style
+// function addTwo(listOne, listTwo) {
+//   const node = new ListNode(0);
+//   let carry = 0;
+//   let dummy = node;
+//   while (listOne || listTwo || carry) {
+//     let l1 = listOne?.val || 0;
+//     let l2 = listTwo?.val || 0;
+//     let sum = l1 + l2 + carry;
+//     carry = Math.floor(sum / 10);
+//     const current = new ListNode(sum % 10);
+//     dummy.next = current;
+//     dummy = current;
+//     listOne = listOne?.next || null;
+//     listTwo = listTwo?.next || null;
+//   }
+//   return node.next;
+// }
+// var addTwoNumbers = function (l1, l2) {
+//   return addTwo(l1, l2);
+// };
+// -----------------------------------------------------------------------------------------------------
+// -----------------------------------------------------------------------------------------------------
